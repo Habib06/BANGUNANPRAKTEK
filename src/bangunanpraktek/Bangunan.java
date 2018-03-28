17bf842cd1fd850e537d2173a2d3e4b04421d8de
Habib06 <habib06@gmail.com>
2018-03-28 12:57:21 +0800

    Membuat Atribut dan Behavior

--- src/bangunanpraktek/Bangunan.java
+++ src/bangunanpraktek/Bangunan.java
@@ -10,5 +10,17 @@ package bangunanpraktek;
  * @author Asus
  */
 public class Bangunan {
+    String Jumlah_Kamar;
+    String Lebar_Bangunan;
+    String Tinggi_Bangunan;
+    String Warna_Bangunan;
+    String Fasilitas;
     
+    void cetakInfo(){
+        System.out.println("Jumlah_Kamar        :"+Jumlah_Kamar);
+        System.out.println("Lebar_Bangunan      :"+Lebar_Bangunan);
+        System.out.println("Tinggi_Bangun       :"+Tinggi_Bangunan);
+        System.out.println("Warna_Bangunan      :"+Warna_Bangunan);
+        System.out.println("Fasilitas           :"+Fasilitas);
+    }
 }
