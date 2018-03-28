06968edd39caa52785843ff6f397f057182dd734
Habib06 <habib06@gmail.com>
2018-03-28 13:09:21 +0800

    Mengisi nilai atribut

--- src/bangunanpraktek/BangunanAksi.java
+++ src/bangunanpraktek/BangunanAksi.java
@@ -13,5 +13,25 @@ public class BangunanAksi {
     public static void main(String[] args) {
         Bangunan Hotel = new Bangunan();
     
+            Hotel.Jumlah_Kamar="50";
+        Hotel.Lebar_Bangunan="40";
+        Hotel.Tinggi_Bangunan="60";
+        Hotel.Warna_Bangunan="Krim";
+        Hotel.Fasilitas="Sarapan";
+        
+        Bangunan Apertemen = new Bangunan();
+        Apertemen.Jumlah_Kamar="6,5";
+        Apertemen.Lebar_Bangunan="5,5";
+        Apertemen.Tinggi_Bangunan="60";
+        Apertemen.Warna_Bangunan="Krim";
+        Apertemen.Fasilitas="Londry";
+        
+        Bangunan KosKosan = new Bangunan();
+        KosKosan.Jumlah_Kamar="30";
+        KosKosan.Lebar_Bangunan="40";
+        KosKosan.Tinggi_Bangunan="6";
+        KosKosan.Warna_Bangunan="Banyak";
+        KosKosan.Fasilitas="Cuci_Motor";
+    }
  
 }
